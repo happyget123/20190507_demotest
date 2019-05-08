9504f3c4816d6792e7dd725045f7650d4b3403de
Dave Lin <daveline@gmail.com>
2019-05-08 09:36:23 +0800

    have to

---
+++ 20190507_demotest/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _20190507_demotest.Models;
+namespace _20190507_demotest.Controllers
+{
+    public class HomeController : Controller
+    {
+        betterbetterEntities db = new betterbetterEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var query = from pick in db.totbooklists.Take(4)
+                        orderby pick.booklistid descending
+                        select pick;
+            return View(query.ToList());
+        }
+    }
+}
